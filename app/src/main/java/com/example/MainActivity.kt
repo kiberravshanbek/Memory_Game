98d19248eb0fb86022af752a0cf1dae3e4b2e1ce
Sarbon_Toshiba <muxtarov.nodir@gmail.com>
2023-10-01 14:46:07 +0500

    git ishlashini tekshirish

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -10,5 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction().replace(R.id.mainActivity,HomeScreenFragment()).commit()
+
+        //salom salom git ishlashi tekshirildi
     }
 }
