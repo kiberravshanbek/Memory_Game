9e9d7a74d50c29d6fbc937f75880dc59f83b34ff
Nurali <rasuljonovnurali04@gmail.com>
2023-10-03 18:52:06 +0500

    Rasuljonovufh

--- app/src/main/java/com/example/GameFragment.kt
+++ app/src/main/java/com/example/GameFragment.kt
@@ -213,7 +213,21 @@ class GameFragment : Fragment(R.layout.fragment_game) {
                     // Toast.makeText(requireContext(), "sucsess", Toast.LENGTH_SHORT).show()
 
                     if (finish == 0) {
-                        showGameOverDialog(setStar())
+                      //  showGameOverDialog(setStar())
+                        val bundle=Bundle()
+
+                        if (easy == "easy") {
+                            bundle.putString("kod","easy")
+                        }
+                        if (medium == "medium") {
+                            bundle.putString("kod","medium")
+                        }
+                        if (hard == "hard") {
+                            bundle.putString("kod","hard")
+                        }
+                        val fragment =WinnerFragment()
+                        fragment.arguments=bundle
+                        parentFragmentManager.beginTransaction().replace(R.id.mainActivity,fragment).commit()
                     }
                     Toast.makeText(requireContext(), "siz yutdingiz", Toast.LENGTH_SHORT).show()
 
